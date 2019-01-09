0f8f65620eda5f34ceb6292ca8ba4bc91de40b96
Adrian Kunz <clashsoft@hotmail.com>
2019-01-09 18:02:09 +0100

    Implement Calculator

--- Rechner/AppDelegate.h
+++ Rechner/AppDelegate.h
@@ -9,7 +9,94 @@
 #import <Cocoa/Cocoa.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
+{
+
+@private double var1;
+@private double var2;
+@private Byte curVar;
+@private NSString* curCal;
+@private BOOL commaMode;
+@private int commaPos;
+@private Byte cMode;
+    
+@private double result;
+    
+    __weak NSTextFieldCell *_TextView;
+    __weak NSTextFieldCell *_ResultTextView;
+    __weak NSTextField *_Console;
+    __weak NSTextField *_ConsoleCommand;
+    
+    __weak NSButton *_Button1;
+    __weak NSButton *_Button2;
+    __weak NSButton *_Button3;
+    __weak NSButton *_Button4;
+    __weak NSButton *_Button5;
+    __weak NSButton *_Button6;
+    __weak NSButton *_Button7;
+    __weak NSButton *_Button8;
+    __weak NSButton *_Button9;
+    __weak NSButton *_Button0;
+    __weak NSButton *_ButtonNeg;
+    __weak NSButton *_ButtonComma;
+    __weak NSButton *_ButtonC;
+    
+    __weak NSButton *_ButtonPlus;
+    __weak NSButton *_ButtonMinus;
+    __weak NSButton *_ButtonMultiplicate;
+    __weak NSButton *_ButtonDivide;
+    
+    __weak NSButton *_ButtonExponent;
+    __weak NSButton *_ButtonSQRT;
+    __weak NSButton *_ButtonReciprocal;
+    __weak NSButton *_ButtonPercent;
+    __weak NSButton *_ButtonSIN;
+    __weak NSButton *_ButtonCOS;
+    __weak NSButton *_ButtonTAN;
+    __weak NSButton *_ButtonADD;
+    __weak NSButton *_ButtonLCM;
+    __weak NSButton *_ButtonGCD;
+    
+    __weak NSButton *_ButtonPI;
+    __weak NSButton *_ButtonE;
+    
+    __weak NSButton *_ButtonEquals;
+}
 
 @property (assign) IBOutlet NSWindow *window;
 
+@property (weak) IBOutlet NSTextFieldCell *TextView;
+
+@property (weak) IBOutlet NSButton *Button1;
+@property (weak) IBOutlet NSButton *Button2;
+@property (weak) IBOutlet NSButton *Button3;
+@property (weak) IBOutlet NSButton *Button4;
+@property (weak) IBOutlet NSButton *Button5;
+@property (weak) IBOutlet NSButton *Button6;
+@property (weak) IBOutlet NSButton *Button7;
+@property (weak) IBOutlet NSButton *Button8;
+@property (weak) IBOutlet NSButton *Button9;
+@property (weak) IBOutlet NSButton *Button0;
+@property (weak) IBOutlet NSButton *ButtonNeg;
+@property (weak) IBOutlet NSButton *ButtonComma;
+@property (weak) IBOutlet NSButton *ButtonPlus;
+@property (weak) IBOutlet NSButton *ButtonMinus;
+@property (weak) IBOutlet NSButton *ButtonMultiplicate;
+@property (weak) IBOutlet NSButton *ButtonDivide;
+@property (weak) IBOutlet NSButton *ButtonExponent;
+@property (weak) IBOutlet NSButton *ButtonSQRT;
+@property (weak) IBOutlet NSButton *ButtonReciprocal;
+@property (weak) IBOutlet NSButton *ButtonPercent;
+@property (weak) IBOutlet NSButton *ButtonSIN;
+@property (weak) IBOutlet NSButton *ButtonCOS;
+@property (weak) IBOutlet NSButton *ButtonTAN;
+@property (weak) IBOutlet NSButton *ButtonEquals;
+@property (weak) IBOutlet NSButton *ButtonPI;
+@property (weak) IBOutlet NSButton *ButtonE;
+@property (weak) IBOutlet NSTextFieldCell *ResultTextView;
+@property (weak) IBOutlet NSButton *ButtonC;
+@property (weak) IBOutlet NSTextField *Console;
+@property (weak) IBOutlet NSTextField *ConsoleCommand;
+@property (weak) IBOutlet NSButton *ButtonADD;
+@property (weak) IBOutlet NSButton *ButtonLCM;
+@property (weak) IBOutlet NSButton *ButtonGCD;
 @end
--- Rechner/AppDelegate.m
+++ Rechner/AppDelegate.m
@@ -7,12 +7,385 @@
 //
 
 #import "AppDelegate.h"
+#import "math.h"
 
 @implementation AppDelegate
 
+
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+	var1 = 0;
+	var2 = 0;
+	curVar = 1;
+	curCal = @"+";
+	commaPos = 0;
+	commaMode = NO;
+	result = 0;
+	cMode = 1;
+}
+
+#pragma mark "Numpad"
+
+- (IBAction)Button1_Press:(id)sender
+{
+	[self addNumber:(1)];
+}
+
+- (IBAction)Button2_Press:(id)sender
+{
+	[self addNumber:(2)];
+}
+
+- (IBAction)Button3_Press:(id)sender
+{
+	[self addNumber:(3)];
+}
+
+- (IBAction)Button4_Press:(id)sender
+{
+	[self addNumber:(4)];
+}
+
+- (IBAction)Button5_Press:(id)sender
+{
+	[self addNumber:(5)];
+}
+
+- (IBAction)Button6_Press:(id)sender
+{
+	[self addNumber:(6)];
+}
+
+- (IBAction)Button7_Press:(id)sender
+{
+	[self addNumber:(7)];
+}
+
+- (IBAction)Button8_Press:(id)sender
+{
+	[self addNumber:(8)];
+}
+
+- (IBAction)Button9_Press:(id)sender
+{
+	[self addNumber:(9)];
+}
+
+- (IBAction)Button0_Press:(id)sender
+{
+	[self addNumber:(0)];
+}
+
+#pragma mark "Basic Operators"
+
+- (IBAction)ButtonNegate_Press:(id)sender
+{
+	if (curVar == 1)
+	{
+		var1 *= -1;
+	}
+	else if (curVar == 2)
+	{
+		var2 *= -1;
+	}
+	[self updateTextView];
+}
+
+- (IBAction)ButtonComma_Press:(id)sender
+{
+	commaMode = true;
+	commaPos = 1;
+}
+
+- (IBAction)ButtonC_Press:(id)sender
+{
+	if (cMode == 1)
+	{
+		if (curVar == 1)
+		{
+			var1 = 0;
+		}
+		else if (curVar == 2)
+		{
+			var2 = 0;
+		}
+		cMode = 2;
+	}
+	else
+	{
+		curVar = 1;
+		var1 = 0;
+		var2 = 0;
+		result = 0;
+		_ResultTextView.stringValue = @"";
+		cMode = 1;
+	}
+	commaMode = NO;
+	commaPos = 0;
+	[self updateTextView];
+}
+
+- (IBAction)ButtonPlus_Press:(id)sender
+{
+	[self setNewCalculateMode:@"+"];
+}
+
+- (IBAction)ButtonMinus_Press:(id)sender
+{
+	[self setNewCalculateMode:@"-"];
+}
+
+- (IBAction)ButtonMultiplicate_Press:(id)sender
+{
+	[self setNewCalculateMode:@"*"];
+}
+
+- (IBAction)ButtonDivide_Press:(id)sender
+{
+	[self setNewCalculateMode:@"/"];
+}
+
+#pragma mark "Advanced Operators"
+
+- (IBAction)ButtonExponent_Press:(id)sender
+{
+	[self setNewCalculateMode:@"^"];
+}
+
+- (IBAction)ButtonSQRT_Press:(id)sender
+{
+	[self setNewCalculateMode:@"√"];
+}
+
+- (IBAction)ButtonReciprocal:(id)sender
+{
+	if (curVar == 1)
+	{
+		var1 = 1 / var1;
+	}
+	else if (curVar == 2)
+	{
+		var2 = 1 / var2;
+	}
+	[self updateTextView];
+}
+
+- (IBAction)ButtonPercent_Press:(id)sender
+{
+}
+
+- (IBAction)ButtonSIN_Press:(id)sender
+{
+	[self setNewCalculateMode:@"sin"];
+}
+
+- (IBAction)ButtonCOS_Press:(id)sender
+{
+	[self setNewCalculateMode:@"cos"];
+}
+
+- (IBAction)ButtonTAN_Press:(id)sender
+{
+	[self setNewCalculateMode:@"tan"];
+}
+
+- (IBAction)ButtonADD_Press:(id)sender
+{
+	[self setNewCalculateMode:(@"∑")];
+}
+
+- (IBAction)ButtonGCD_Press:(id)sender
+{
+	[self setNewCalculateMode:(@"GCD")];
+}
+
+- (IBAction)ButtonLCM_Press:(id)sender
+{
+	[self setNewCalculateMode:(@"LCM")];
+}
+
+#pragma mark "Constants"
+
+- (IBAction)ButtonPI_Press:(id)sender
+{
+	if (curVar == 1)
+	{
+		var1 = M_PI;
+	}
+	else if (curVar == 2)
+	{
+		var2 = M_PI;
+	}
+	[self updateTextView];
+}
+
+- (IBAction)ButtonE_Press:(id)sender
+{
+	if (curVar == 1)
+	{
+		var1 = M_E;
+	}
+	else if (curVar == 2)
+	{
+		var2 = M_E;
+	}
+	[self updateTextView];
+}
+
+#pragma mark "Equals"
+
+- (IBAction)ButtonEquals_Press:(id)sender
+{
+	[self calculate:var1 mode:curCal second:var2];
+}
+- (IBAction)CommandTextView_TextChanged:(id)sender
+{
+	NSArray *args = [_ConsoleCommand.stringValue componentsSeparatedByString:@" "];
+	NSString *s = args[0];
+	if ([s isEqual: @"calculate"])
+	{
+		if (args.count != 4)
+		{
+			[self addDeveloperInfo:(@"Too many or to few arguments!")];
+		}
+		else
+		{
+			double d1 = [args[1] doubleValue];
+			double d2 = [args[3] doubleValue];
+			[self calculate:d1 mode:args[2] second:d2];
+		}
+	}
+	else
+	{
+		[self addDeveloperInfo:@"Unknown console command!"];
+	}
+}
+
+- (void)addNumber:(int)number
+{
+	if (!commaMode)
+	{
+		if (curVar == 1)
+		{
+			var1 *= 10;
+			var1 += number;
+		}
+		else if (curVar == 2)
+		{
+			var2 *= 10;
+			var2 += number;
+		}
+	}
+	else
+	{
+		double d = pow(0.1, commaPos);
+		if (curVar == 1)
+		{
+			var1 += number * d;
+			commaPos++;
+		}
+		else if (curVar == 2)
+		{
+			var2 += number * d;
+			commaPos++;
+		}
+	}
+	cMode = 1;
+	[self updateTextView];
+}
+
+- (void)updateTextView
+{
+	_TextView.stringValue = [NSString stringWithFormat:(@"%@\n%@ %@"), [[NSNumber numberWithDouble:var1] stringValue], curCal, [[NSNumber numberWithDouble:var2] stringValue]];
+
+}
+
+- (void)setNewCalculateMode:(NSString*)mode
+{
+	commaMode = NO;
+	commaPos = 0;
+	if (curVar == 1)
+	{
+		curVar = 2;
+		curCal = mode;
+	}
+	else if (curVar == 2)
+	{
+		var1 = [self calculate:var1 mode:curCal second:var2];
+		var2 = 0;
+		curVar = 2;
+		curCal = mode;
+	}
+	[self updateTextView];
+}
+
+- (double)calculate:(double)par1 mode:(NSString*)mode second:(double)par2
+{
+	double d = 0;
+	if ([mode isEqual: @"+"]) {d = par1 + par2;}
+	else if ([mode isEqual: @"-"]) {d = par1 - par2;}
+	else if ([mode isEqual: @"*"]) {d = par1 * par2;}
+	else if ([mode isEqual: @"/"]) {d = par1 / par2;}
+	else if ([mode isEqual: @"^"]) {d = pow(par1, par2);}
+	else if ([mode isEqual: @"√"]) {d = [AppDelegate sqrt:par2 number:par1];}
+	else if ([mode isEqual: @"∑"]) {for (int i = par1; i <= par2; i++) { d += i; };}
+	else if ([mode isEqual: @"sin"]) {d = sin(par1);}
+	else if ([mode isEqual: @"cos"]) {d = cos(par1);}
+	else if ([mode isEqual: @"tan"]) {d = tan(par1);}
+	else if ([mode isEqual: @"LCM"]) {d = [AppDelegate lcm:par1 and:par2];}
+	else if ([mode isEqual: @"GCD"]) {d = [AppDelegate gcd:par1 and:par2];}
+
+	result = d;
+	_ResultTextView.stringValue = [NSString stringWithFormat:(@"%@"), [[NSNumber numberWithDouble:result] stringValue]];
+	[self addDeveloperInfo:([NSString stringWithFormat:(@"%@ %@ %@ = %@"), [[NSNumber numberWithDouble:par1] stringValue], curCal, [[NSNumber numberWithDouble:par2] stringValue], [[NSNumber numberWithDouble:result] stringValue]])];
+
+	return d;
+}
+
++ (double)sqrt:(double)par1 number:(double)par2
+{
+	return pow(par1, 1 / par2);
+}
+
++ (int)lcm:(int)a and:(int)b
+{
+	int n;
+	for(n=2;;n++)
+	{
+		if(n % a == 0 && (n % b == 0))
+		{
+			return n;
+		}
+	}
+	return 0;
+}
+
++ (int)gcd:(int)a and:(int)b
+{
+	int c;
+	if(a<b)
+	{
+		c = a;
+		a = b;
+		b = c;
+	}
+	while(1)
+	{
+		c = a % b;
+		if(c==0)
+		{
+			return b;
+		}
+		a = b;
+		b = c;
+	}
+	return 0;
+}
+
+- (void)addDeveloperInfo:(NSString*)info
+{
+	_Console.stringValue = [NSString stringWithFormat:(@"%@%@%c"), _Console.stringValue, info, '\n'];
 }
 
 @end
